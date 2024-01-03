41b6ba81a7ef40d283b7f249290acdf3f79a74b0
SOUBHIK BISWAS <54506603+SoubhikBiswas-gitHub@users.noreply.github.com>
2024-01-03 18:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let answer = confirm("Do you want to proceed?");
+
+if (answer) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
